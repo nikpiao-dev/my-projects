6921480325dc1051bb0a22fc1f4b0e65c3b591e2
Nik <nik.piao26@gmail.com>
2025-05-11 17:02:37 -0400

    Added more info and code in terminal_game.py file

--- codedex/legends_python/terminal_game.py
+++ codedex/legends_python/terminal_game.py
@@ -1,3 +1,41 @@
 # Checkpoint Project - Terminal Game 
 
-print("Hello, World")
+"""
+🧝 FANTASY ADVENTURE:
+
+You’re a wandering hero in a kingdom plagued by dark magic. 
+
+The king is missing, and an ancient evil is rising.
+
+A MINI-RPG: A Day in the Life of John the Wanderer
+
+Your Stats:
+
+🗡️ Attack Power: 5
+
+🛡️ Block Value: 3
+
+❤️ HP (Health Points): 20
+
+✋ Heal: Restore 5 HP (limit: 3 uses per day)
+
+
+Things to do (example):
+
+    A) Visit the market to prepare for a journey.
+    B) Go to the forest—you heard whispers of something stirring.
+    C) Stay in the inn and talk to the old innkeeper.
+"""
+
+title = "Path of the Wanderer: The Journey of Rowen of Elderfall"
+genre = "Fantasy Adventure"
+name = "Rowen Ashvale"
+town = "Elderfall"
+
+# Stats
+attack = 5
+block = 3
+hp = 20
+heal = 5 # limit 3 per day (5 hp * 3 = 15 hp/day)
+
+
