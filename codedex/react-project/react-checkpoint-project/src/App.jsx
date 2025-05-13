50a5571ecd53c6029d0bcd605f18f72a099363af
Nik <nik.piao26@gmail.com>
2025-05-13 18:04:50 -0400

    Added react-project folder and files

---
+++ codedex/react-project/react-checkpoint-project/src/App.jsx
@@ -0,0 +1,16 @@
+// Color Picker - Checkpoint Project React
+
+import ColorPicker from './ColorPicker.jsx';
+
+
+function App() {
+  return (
+    <div>
+      <h1>Hello world</h1>
+      <ColorPicker/>
+    </div>
+
+  );
+}
+
+export default App
