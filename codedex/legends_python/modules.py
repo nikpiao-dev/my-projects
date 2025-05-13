5ae478e7d8ad9a2853271f08dd9183f864d7c1aa
Nik <nik.piao26@gmail.com>
2025-05-13 16:40:44 -0400

    Added modules folder and module py file. Also, created an venv folder as well

---
+++ codedex/legends_python/modules.py
@@ -0,0 +1,39 @@
+# The Zen of Python, by Tim Peters
+
+# import this
+
+"""
+
+Beautiful is better than ugly.
+Explicit is better than implicit.
+Simple is better than complex.
+Complex is better than complicated.
+Flat is better than nested.
+Sparse is better than dense.
+Readability counts.
+Special cases aren't special enough to break the rules.
+Although practicality beats purity.
+Errors should never pass silently.
+Unless explicitly silenced.
+In the face of ambiguity, refuse the temptation to guess.
+There should be one-- and preferably only one --obvious way to do it.
+Although that way may not be obvious at first unless you're Dutch.
+Now is better than never.
+Although never is often better than *right* now.
+If the implementation is hard to explain, it's a bad idea.
+If the implementation is easy to explain, it may be a good idea.
+Namespaces are one honking great idea -- let's do more of those!
+
+"""
+
+# pip install wikipedia
+import wikipedia
+
+# Get the summary of a specific topic
+summary = wikipedia.summary('Philosophy of life')
+
+# Print the summary
+print(summary)
+
+# Search for related topics
+print(wikipedia.search('Philosophy of life'))
