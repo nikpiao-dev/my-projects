8d502fb86666c0de66e29816d7c1e6ba61ebd245
Nik <nik.piao26@gmail.com>
2025-05-18 22:03:58 -0400

    Added wav file and deleted a txtfile

---
+++ codedex/origins_trilogy/js/final_project/app.js
@@ -0,0 +1,6 @@
+const bells = new Audio('bell.wav'); 
+const startBtn = document.querySelector('.btn-start'); 
+const session = document.querySelector('.minutes'); 
+let myInterval; 
+let state = true;
+
