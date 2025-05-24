5556b5314a57b5a0826227320070c674bc8cf67c
Nik <nik.piao26@gmail.com>
2025-05-23 20:53:56 -0400

    added app.js into server folder in node_JS

---
+++ codedex/node_JS/servers/tiny_server/app.js
@@ -0,0 +1,16 @@
+const http = require('http');
+
+// Create a server object
+
+const server = http.createServer((request, response) => {
+  response.write('Hello, World!');
+  response.end();
+});
+
+
+server.listen(3000);
+
+
+// Go to http://localhost:3000
+
+// Ctrl + C to exit server
