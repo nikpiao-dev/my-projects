a8cb64dad7cd50744d0ac6cbcbab876d58278aae
Nik <nik.piao26@gmail.com>
2025-05-24 22:02:40 -0400

    Added informational comments in the server.js file :

--- codedex/node_JS/servers/tiny_server/server.js
+++ codedex/node_JS/servers/tiny_server/server.js
@@ -1,14 +1,18 @@
-// Create our own new server
+// Emoji Story - Servers section
+// Codedex - Node.js course
 
-const http = require('http');
+const http = require('http');  // Import the http module
 
+// Create a server
 const server = http.createServer((request, response) => {
     console.log(`Received request: ${request.method} ${request.url}`);
-    
+
     response.writeHead(200, {'Content-Type': 'text/plain; charset=utf-8'});
     response.end('🛒🎥🍽🛍🛋🎞');
 });
 
+
+// Start the server and listen on port 3000
 server.listen(3000, () => {
     console.log('Server running at http://localhost:3000/');
 });
