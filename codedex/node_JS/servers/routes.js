8a07fb403c76ecea119e83cacf85c9d6bd6ac882
Nik <nik.piao26@gmail.com>
2025-05-27 21:54:22 -0400

    added more content to the routes.js file

--- codedex/node_JS/servers/routes.js
+++ codedex/node_JS/servers/routes.js
@@ -14,10 +14,28 @@ const server = http.server((res, req) => {
     let contentType = 'text/html; charset=utf-8';
     let content = '';
 
+    // Content based on the requested route
+    if (req.url === '/') {
+        content = '<h1>Dream House</h1><p>A special place where your dreams find a home and every moment feels like a fresh start.</p>';
+    
+    } else if (req.url === '') {
 
-    // If the route doesn't match, send a 404 error
-    statusCode = 404;
-    content = '<h1>404 Not Found</h1><p>Page does not exist.</p>';
+    } else if (req.url === '') {
+
+    } else if (req.url === '') {
+
+    } else if (req.url === '') {
+
+    } else if (req.url === '') {
+
+    } else if (req.url === '') {
+
+    } else {
+        // If the route doesn't match, send a 404 error
+        statusCode = 404;
+        content = '<h1>404 Not Found</h1><p>Page does not exist.</p>';
+    }
+
 
     // final response
     res.writeHead(statusCode, {'Content-Type': contentType});
