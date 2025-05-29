c2bed78669ba075663a8633854a6f2cf18642625
Nik <nik.piao26@gmail.com>
2025-05-28 21:08:17 -0400

    Finished code revisions and content in pokemon.js file

--- codedex/node_JS/servers/pokemon.js
+++ codedex/node_JS/servers/pokemon.js
@@ -12,28 +12,44 @@ const server = http.createServer((req, res) => {
 
     // Default response settings
     let statusCode = 200;
-    let contentType = 'text/html; charset=utf-8';
+    let textType = 'text/html; charset=utf-8';
+    let contentType = 'text/html'
     let content = '';
 
     if (req.url === '') {
         content = '<h1>Pokémon Center</h1><br><p>A special place where Pokémon find care, healing, and trainers get a fresh start for their journey.</p>';
-    } else if (req.url === 'pikachu') {}
-    else if (req.url === 'sylveon') {}
-    else if (req.url === '') {}
-    else if (req.url === '') {}
-    else if (req.url === '') {}
-
-    else {
+    } else if (req.url === '/pikachu') {
+        console.log("Pika Pika!");
+        content = `
+            <h1>Pikachu's Room</h1>
+            <img src="https://assets.pokemon.com/assets/cms2/img/pokedex/full/025.png" alt="Pikachu" style="width:300px;">
+            <p>Pikachu is feeling energetic!</p>
+        `;
+    } else if (req.url === '/sylveon') {
+        console.log("Sylv Sylv!");
+        content = `
+            <h1>Sylveon's Room</h1>
+            <img src="https://assets.pokemon.com/assets/cms2/img/pokedex/full/700.png" alt="Sylveon" style="width:300px;">
+            <p>Sylveon is relaxing gracefully.</p>
+        `;
+    } else {
         // If the route doesn't match, send a 404 error
         statusCode = 404;
-        content = '<h1>404 Not Found</h1><p>Page does not exist.</p>';
+        content = `
+            <h1>404 Not Found</h1>
+            <p>This Pokémon is currently resting!</p>
+        `;
     } 
-    res.writeHead(statusCode, {'Content-Type': contentType})
-    res.end(content);
+
+    // Set the header and status code
+    res.writeHead(statusCode, { 'Content-Type': contentType });
+
+    res.write(content)
+    res.end('Gotta code ’em all! 🐦‍🔥');
 
 });
 
 server.listen(3000, () => {
-    console.log('Pokemon Center starts at http://localhost:3000/');
+    console.log('Visit Pokemon Center at http://localhost:3000/');
 });
 
