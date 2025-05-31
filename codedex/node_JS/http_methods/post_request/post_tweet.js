fe69ae4906279e4e3041c5fd4c5d3c96c05b3713
Nik <nik.piao26@gmail.com>
2025-05-30 22:24:32 -0400

    FInished the content for post_tweet.js file

---
+++ codedex/node_JS/http_methods/post_request/post_tweet.js
@@ -0,0 +1,25 @@
+const http = require('http');
+
+const server = http.createServer((request, response) => {
+    if (request.method === 'POST') {
+        let message = '';
+
+        request.on('data', (chunk) => {
+            message += chunk;
+        });
+
+        request.on('end', () => {
+            console.log('New message:', message);
+
+            response.writeHead(200, { 'Content-Type': 'text/plain' });
+            response.end('Message received!');
+        });
+    } else {
+        response.writeHead(404, { 'Content-Type': 'text/plain' });
+        response.end('Go back to your terminal!');
+  }
+});
+
+server.listen(3000, () => {
+    console.log('Server is running at http://localhost:3000');
+});
