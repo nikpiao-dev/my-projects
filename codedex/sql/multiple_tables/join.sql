531e6a1759b2cf1d509691fd8bb29c621b3a5c95
Nik <nik.piao26@gmail.com>
2025-06-02 19:05:00 -0400

    added a new sql file called join.sql

---
+++ codedex/sql/multiple_tables/join.sql
@@ -0,0 +1,13 @@
+-- Here are some of the tables that we'll be working with later in this chapter:
+
+-- movies and directors, books and authors, students and teachers 
+
+-- Use SELECT * to return each of the tables and get familiar with what’s inside!
+
+
+-- select * from movies;
+-- select * from directors;
+-- select * from books;
+-- select * from authors;
+-- select * from students;
+select * from teachers;
