0bc465a3c49986df177029082e93f79029b8a3ae
Nik <nik.piao26@gmail.com>
2025-06-03 20:33:12 -0400

    added a boilerplate code inside

---
+++ codedex/node_JS/http_methods/delete_request/delete.js
@@ -0,0 +1,13 @@
+// Use what we learned above to create a status message 
+// and then delete it using the DELETE request.
+
+const http = require('http');
+
+
+const server = http.createServer((req, res) => {
+
+});
+
+server.listen(3000, () => {
+    console.log('Server running on http://localhost:3000');
+});
