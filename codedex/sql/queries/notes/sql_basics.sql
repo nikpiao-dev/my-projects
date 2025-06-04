8289617821b5810e7d8886726e0c7ef47de746b7
Nik <nik.piao26@gmail.com>
2025-06-04 16:19:49 -0400

    added new sql file

---
+++ codedex/sql/queries/notes/sql_basics.sql
@@ -0,0 +1,13 @@
+-- SQL Basics Notes --
+
+SELECT column1, column2 FROM table_name;
+SELECT * FROM table_name;
+
+ -- WHERE Clause
+SELECT * FROM movies WHERE rating > 8;
+
+--  ORDER BY
+SELECT * FROM movies ORDER BY release_year DESC;
+
+-- LIMIT
+SELECT * FROM movies LIMIT 5;
