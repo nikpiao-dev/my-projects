c688224efdab480b6399bf44bc200ad4bdcad456
Nik <nik.piao26@gmail.com>
2025-06-04 20:16:18 -0400

    added new content to sql file called lef_join.sql

--- codedex/sql/multiple_tables/left_join/left_join.sql
+++ codedex/sql/multiple_tables/left_join/left_join.sql
@@ -13,6 +13,25 @@ FROM table1
 LEFT JOIN table2
   ON table1.column1 = table2.column2;
 
+-- Using Inner Join
+SELECT name, year, country
+FROM directors
+INNER JOIN movies
+  ON directors.name = movies.director;
+
+
+-- NOTES: So what’s the difference between JOIN and LEFT JOIN?
+
+JOIN:
+
+-- Returns rows where there’s a match in both tables.
+-- If there’s no match, that row is excluded.
+
+LEFT JOIN:
+
+-- Returns all rows from the left table, and matched rows from the right.
+-- If there’s no match on the right, you’ll still get the left row... but with NULLs for the right-side columns.
+
 
 
 -- select both tables
