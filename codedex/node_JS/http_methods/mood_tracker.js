017603985883c629705517ac4d2278803fb0160a
Nik <nik.piao26@gmail.com>
2025-06-04 20:31:27 -0400

    added new txt file within sql folder and created a new js file called mood_tracker.js

---
+++ codedex/node_JS/http_methods/mood_tracker.js
@@ -0,0 +1,13 @@
+// Import http module
+const http = require('http');
+
+
+// 
+
+const server = http.createServer((response, request) => {
+
+});
+
+server.listen(3000, () => {
+    console.log('Server running on http://localhost:3000');
+});
