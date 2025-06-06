3eac78ba6a4dac6448f6e088a719f3c399e39693
Nik <nik.piao26@gmail.com>
2025-06-06 00:18:54 -0400

    added new content within both sql files

---
+++ codedex/sql/multiple_tables/union/student_teachers.sql
@@ -0,0 +1,9 @@
+
+-- Students & Teachers 🏫
+-- Codédex
+
+SELECT name
+FROM students
+UNION
+SELECT name
+FROM teachers;
--- codedex/sql/multiple_tables/union/union.sql
+++ codedex/sql/multiple_tables/union/union.sql
@@ -1,5 +1,19 @@
--- select both tables to check data:
+-- Instructions:
 
+-- We have a students table and a teachers table in a school database ready for you.
+-- Use SELECT to see what they look like.
+-- Let's do a roll call. Can you use UNION to get all the names from both tables?
+
+
+
+-- Example:
+SELECT name
+FROM directors
+UNION
+SELECT name
+FROM movies;
+
+-- select both tables to check data:
 select * 
 from students 
 limit 5;
@@ -8,3 +22,12 @@ select *
 from teachers 
 limit 5;
 
+
+
+-- USE union operator to combine two tables into one list w/o duplicates:
+
+SELECT name 
+FROM students
+UNION
+SELECT name
+FROM teachers;
