84b6cd901aea6662e598c77e078ab90ac3da1130
Nik <nik.piao26@gmail.com>
2025-06-06 00:22:18 -0400

    added 2 files (sql and txt file) for the final lesson of codedex sql course

---
+++ codedex/sql/multiple_tables/online_shop.sql
@@ -0,0 +1,34 @@
+-- Instructions:
+
+-- Imagine you run an online Shopify store and have to manage all the orders, products, and customers.
+
+-- screenshot
+
+-- The data are all kept in three tables.
+
+-- orders has the following columns:
+
+-- id
+-- date
+-- customer
+-- customer_id
+-- fulfilled
+-- item_id
+-- products has the following columns:
+
+-- id
+-- name
+-- price
+-- active
+-- inventory
+-- customers has the following columns:
+
+-- id
+-- name
+-- location
+-- email_subscriber
+-- Use joins and what we learned in previous chapters to find insights about the data. You can formulate your own questions, but here are some of ours:
+
+-- What are the best selling items? What are the most underperforming ones?
+-- What are all the orders the customers bought?
+-- What days had the highest sales? And the lowest?
