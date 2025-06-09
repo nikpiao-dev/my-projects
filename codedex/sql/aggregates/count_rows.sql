fbb48c41c782904d3b55a48cf27158a97c3844c9
Nik <nik.piao26@gmail.com>
2025-06-08 22:58:41 -0400

    added nw file called count_rows.sql

---
+++ codedex/sql/aggregates/count_rows.sql
@@ -0,0 +1,12 @@
+-- # COUNT() function counts the number of rows:
+
+SELECT COUNT(*)
+FROM table_name;
+
+-- This returns the total number of rows within a table.
+
+
+-- Find the total number of rows in the playlist table with COUNT().
+
+SELECT COUNT(*)
+FROM playlist;
