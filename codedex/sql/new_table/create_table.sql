fcf546368dc7c3be2108eeeb99a0e24dd9f0c718
Nik <nik.piao26@gmail.com>
2025-06-10 01:24:45 -0400

    added new sql and txt files within the new_table folder

---
+++ codedex/sql/new_table/create_table.sql
@@ -0,0 +1,28 @@
+-- CREATE TABLE statement creates a brand new table in a database.
+
+-- Example) we are creating a new table called companies:
+CREATE TABLE companies (
+  id INTEGER,
+  name TEXT,
+  headquarters TEXT,
+  year INTEGER
+);
+
+-- Here, we are creating a new table of companies with the following columns:
+
+-- id column of the data type INTEGER.
+-- name column of the data type TEXT.
+-- headquarters column of the data type TEXT.
+-- year column of the data type INTEGER.
+
+
+-- -- DATA Types:
+-- All the data stored in a database has a type. The data type lets the database know what to expect from each column and determines the kind of interactions that can happen.
+
+-- Some of the most common data types are:
+
+-- TEXT: a text string
+-- INTEGER: a positive or negative whole number
+-- REAL: a positive or negative decimal number
+-- DATE: a date format (YYYY-MM-DD)
+
