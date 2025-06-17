f220d4fed74bd5679009b800e42f65eacc15d8ac
Nik <nik.piao26@gmail.com>
2025-06-17 15:27:25 -0400

    added new folder called intermediate js with additional contents inside

---
+++ codedex/origins_trilogy/js/intermediate-js/ecmascript/arrow_functions/index.js
@@ -0,0 +1,11 @@
+// Rewrite as an arrow function 💖
+
+// function forceEquation(mass, acceleration) {
+//   return mass * acceleration;
+// }
+
+// console.log(forceEquation(3, 4));
+
+
+const forceEquation = (mass, acceleration) => mass * acceleration;
+console.log(forceEquation(3,4));
