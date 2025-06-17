bf64d690493d06646abfc00395ebff15d987da03
Nik <nik.piao26@gmail.com>
2025-06-17 15:29:44 -0400

    added a new folder with contents about js

---
+++ codedex/origins_trilogy/js/intermediate-js/template_literals/index.js
@@ -0,0 +1,10 @@
+// Change these to string template literals 💖
+
+for (let i = 99; i > 0; i--) {
+  console.log(`${i} bottles of pop on the wall`);
+  console.log(`${i} bottles of pop`)
+  console.log("take one down, pass it around");
+  console.log(`${i - 1} bottles of pop pn the wall`);
+  console.log();
+}
+
