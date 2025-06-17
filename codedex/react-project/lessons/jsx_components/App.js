e3cef8d059cc82e2469d19953081fcd858b43b8c
Nik <nik.piao26@gmail.com>
2025-06-17 15:19:58 -0400

    added a new js file and made changes to the older App.js file

--- codedex/react-project/lessons/jsx_components/App.js
+++ codedex/react-project/lessons/jsx_components/App.js
@@ -1,14 +1,9 @@
 export default function App() {
+  const name = 'nicholas';
   return (
-    <div>
-      <h1>Tour Stops</h1>
-      <ul>
-        <li>🇺🇸 New York City, US - Madison Square Garden</li>
-        <li>🇬🇧 London, UK - Wembley Stadium</li>
-        <li>🇩🇪 Munich, DE - Zenith Halle</li>
-        <li>🇯🇵 Tokyo, JP - Budokan</li>
-        <li>🇦🇺 Melbourne, AU - The Forum</li>
-      </ul>
-    </div>
+    <div>My name is {name}!</div>
   )
 }
+
+
+
