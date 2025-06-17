a509611de981129f7632e05308cb7896efdd3eb5
Nik <nik.piao26@gmail.com>
2025-06-17 16:12:50 -0400

    added a new foler with bunch of contents within the legends_of_python section

---
+++ codedex/legends_python/intermediate_python/data_structures/intro/script.py
@@ -0,0 +1,15 @@
+# Instructions:
+
+# First, a quick refresh on lists. 📋
+
+# In a world where social media usage makes up 2+ hours of our day on average, it's hard to keep up with slang. Let's build a list of your favorite trendy or overused expressions! 😎
+
+# For example:
+
+# genz_slang = ['bestie', 'tea', 'cap', 'sus']
+
+# Then, go ahead and print out your list!
+
+
+genz_slang = ['bestie', 'tea', 'cap', 'sus']
+print(genz_slang)
