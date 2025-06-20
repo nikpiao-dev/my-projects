5db1ab386ac79d801004af5e60110c2005f0d173
Nik <nik.piao26@gmail.com>
2025-06-19 20:32:19 -0400

    added more content within the App.js file in components and created a new folder called props_and_state with addtional contents inside

--- codedex/react-project/lessons/jsx_components/components/App.js
+++ codedex/react-project/lessons/jsx_components/components/App.js
@@ -1,10 +1,12 @@
 import Header from "./Header.js";
+import Reviews from "./Reviews.js";
 import Footer from "./Footer.js";
 
 export default function App() {
   return (
     <div>
       <Header />
+      <Reviews />
       <Footer />
     </div>
   );
---
+++ codedex/react-project/lessons/props_and_state/App.js
@@ -0,0 +1,5 @@
+export default function App() {
+  // const catalogItems = array of objects here 💖
+  return <div>{/* Write code here 💖 */}</div>;
+}
+
