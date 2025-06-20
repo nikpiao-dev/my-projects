f31dd905c989a3d690b1ff3959aabc06415404df
Nik <nik.piao26@gmail.com>
2025-06-19 20:36:44 -0400

    changes in the content of App.js file

--- codedex/react-project/lessons/props_and_state/App.js
+++ codedex/react-project/lessons/props_and_state/App.js
@@ -1,5 +1,32 @@
 export default function App() {
-  // const catalogItems = array of objects here 💖
-  return <div>{/* Write code here 💖 */}</div>;
+    const catalogItems = [
+    {
+      name: "Dan",
+      category: "Developer",
+      website: "dandeveloper.dev"
+    },
+    {
+      name: "Fatima",
+      category: "Doctor",
+      website: "fatimahealth.com"
+    },
+    {
+      name: "Juan",
+      category: "Community Leader",
+      website: "juan.me"
+    }
+  ]
+  return (
+    <div>
+      {catalogItems.map(function (item) {
+        return (
+          <div>
+            <h2>{item.name}</h2>
+            <h3>Specialty: {item.category}</h3>
+            <a href={item.website}>Learn more</a>
+          </div>
+        )
+      })}
+    </div>
+  )
 }
-
