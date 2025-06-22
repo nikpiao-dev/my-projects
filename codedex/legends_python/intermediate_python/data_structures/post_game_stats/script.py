d8425d4f545a49c4ea501c51559952540f31c2be
Nik <nik.piao26@gmail.com>
2025-06-22 01:02:27 -0400

    added new files to the folder

---
+++ codedex/legends_python/intermediate_python/data_structures/post_game_stats/script.py
@@ -0,0 +1,22 @@
+"""
+
+Instructions:
+
+Imagine you have a dataset 📊 with information about your favorite sports team. 🏀🎾⚽ The goal is to use Python's data structures to organize and analyze this data.
+
+If you can't think of any, feel free to use the 🏈 Super Bowl 2024 champions, the Kansas City Chiefs. 🏆
+
+As a data analyst for the Kansas City Chiefs you have been given a dataset containing information about the players, their positions, and some game statistics.
+
+Let's start analyzing!
+
+Create a list of dictionaries where each dictionary represents a player. Include attributes such as 'name,' 'position,' and 'jersey number.'
+Print out a list of all player positions in the dataset.
+Choose a player and update their game statistics in the dataset.
+Calculate the average statistics (e.g., yards gained, touchdowns) for all players and print the results.
+Congratulations for reaching the end of this chapter! Using data structures, you were able to organize and analyze data for the Chiefs. 😎 Think of all the things you can analyze!
+
+✨ Just remember practice makes perfect. ✨
+
+"""
+
