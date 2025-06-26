06c8236fb86a3115f8b6db9ba68924d9b0459068
Nik <nik.piao26@gmail.com>
2025-06-26 16:23:49 -0400

    added more content in the script.py file

--- codedex/legends_python/intermediate_python/file_and_error_handling/uncharted_files/script.py
+++ codedex/legends_python/intermediate_python/file_and_error_handling/uncharted_files/script.py
@@ -35,3 +35,32 @@ Note: seek(0) is used to move the cursor to the beginning of the file before mod
 Then, truncate() is used to adjust the file size to match the length of the new message. This simulates the act of unsending the message within the file.
 
 """
+
+
+
+sent_message = 'Hey there! This is a secret message.'
+
+with open('sent_message.txt', 'w') as file:
+  file.write(sent_message)
+
+with open('sent_message.txt', 'r+') as file:
+  # Read the sent message from the file
+  original_message = file.read()
+
+  # Move the cursor to the beginning of the file
+  file.seek(0)
+
+  # Modify the message to simulate unsending
+  unsent_message = 'This message has been unsent.'
+  
+  # Truncate the file to the length of the modified message
+  file.truncate(len(unsent_message))
+
+  # Write the modified message to the file
+  file.write(unsent_message)
+
+  # Display and print both messages 
+  print('Original Message:', original_message)
+  print('Unsent Message:', unsent_message)
+
+
