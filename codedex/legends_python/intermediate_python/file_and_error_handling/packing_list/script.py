514d36cc00f685703167303b9fb8ecbf4c91b75a
Nik <nik.piao26@gmail.com>
2025-06-27 00:02:03 -0400

    added new content in script.py file

--- codedex/legends_python/intermediate_python/file_and_error_handling/packing_list/script.py
+++ codedex/legends_python/intermediate_python/file_and_error_handling/packing_list/script.py
@@ -38,3 +38,25 @@ Happy packing! 📦
 
 """
 
+
+import csv
+
+data = [
+  ['Item', 'Quantity'],
+  ['Blender', 2],
+  ['Posters', 30],
+  ['Shoes', 2]
+]
+
+with open('packing_list.csv', 'r') as csv_file:
+  csv_reader = csv.reader(csv_file)
+
+  try:
+    for row in csv_reader:
+      print(row)
+  except FileNotFoundError:
+    print("Packing list file not found.")
+    with open('packing_list.csv', 'w', newline='') as csv_file:
+      csv_writer = csv.writer(csv_file)
+
+      csv_writer.writerows(data)
