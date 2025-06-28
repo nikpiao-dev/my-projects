9fea83ba04774e1a48e20ed4f4e20a7d470f8503
Nik <nik.piao26@gmail.com>
2025-06-28 00:22:07 -0400

    added new content within the script.py file

--- codedex/legends_python/intermediate_python/functional_programming/grammys/script.py
+++ codedex/legends_python/intermediate_python/functional_programming/grammys/script.py
@@ -20,3 +20,46 @@ A minutes_to_seconds() function for map().
 An add_durations() function for reduce().
 
 """
+
+from functools import reduce
+
+
+def n1(): # function for new line space
+  print('\n')
+
+# List of songs with their durations (in minutes)
+playlist = [('What Was I Made For?', 3.42), ('Just Like That', 5.05), ('Song 3', 6.8), ('Leave The Door Open', 4.02), ('I Can\'t Breath', 4.47), ('Bad Guy', 3.14)]
+
+
+def is_long(song):
+  return song[1]> 5
+
+# def minutes_to_seconds(song):
+#   title, duration = song
+#   return [title, duration * 60] 
+
+def minutes_to_seconds(song):
+  duration = song[1]
+  minutes = int(duration)
+  seconds = (duration - minutes) * 100
+
+  return minutes * 60 + round(seconds)
+        
+longer_songs = filter(is_long, playlist)
+print("Longer songs:", list(longer_songs))
+
+n1()
+
+songs_in_seconds = map(minutes_to_seconds, playlist)
+print("Duration of songs(seconds):", list(songs_in_seconds))
+
+
+n1()
+
+def add_durations(total, song):
+    # return total + song[1]  
+    duration = song[1]
+    return total + duration
+
+total_playtime = reduce(add_durations, playlist, 0)
+print("Total Playtime:", total_playtime)
