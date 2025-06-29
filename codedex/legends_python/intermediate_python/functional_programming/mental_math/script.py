7ccd0fd165f5ebc58679e8560f6895f70335c091
Nik <nik.piao26@gmail.com>
2025-06-28 20:17:50 -0400

    added new content within the script python file

--- codedex/legends_python/intermediate_python/functional_programming/mental_math/script.py
+++ codedex/legends_python/intermediate_python/functional_programming/mental_math/script.py
@@ -18,3 +18,13 @@ Bonus: create and print a list of odd numbers
 """
 
 
+
+numbers = [57, 10, 82, 36, 89, 46, 13, 23, 92, 48]
+
+even_numbers = [num for num in numbers if num % 2 == 0]
+odd_numbers = [num for num in numbers if num % 2 != 0]
+
+
+print("Original Numbers:", numbers)
+print("Even Numbers:", even_numbers)
+print("Odd Numbers:", odd_numbers)
