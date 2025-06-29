26d68dab3421e621aeadd09e9feba36e72e7e52f
Nik <nik.piao26@gmail.com>
2025-06-29 00:09:31 -0400

    added new content within the App.js file

--- codedex/react-project/lessons/props_and_state/App.js
+++ codedex/react-project/lessons/props_and_state/App.js
@@ -69,3 +69,33 @@ import Quiz from "./Quiz.js"
 export default function App() {
   return <Quiz />;
 }
+
+// TrendingList and Movies
+
+import { useState } from "react";
+import TrendingList from "./TrendingList";
+
+export default function App() {
+  const movieArray = [
+    {
+      title: "Hitchhiker's Guide to the Galaxy",
+      releaseYear: 2005,
+      imageUrl: "https://upload.wikimedia.org/wikipedia/en/7/7a/Hitchhikers_guide_to_the_galaxy.jpg"
+    },
+    {
+      title: "Hitchhiker's Guide to the Galaxy",
+      releaseYear: 2005,
+      imageUrl: "https://upload.wikimedia.org/wikipedia/en/7/7a/Hitchhikers_guide_to_the_galaxy.jpg"
+    },
+    {
+      title: "Hitchhiker's Guide to the Galaxy",
+      releaseYear: 2005,
+      imageUrl: "https://upload.wikimedia.org/wikipedia/en/7/7a/Hitchhikers_guide_to_the_galaxy.jpg"
+    }    
+  ];
+  const [movieData, setMovieData] = useState(movieArray);
+
+  // code here 💖
+
+  return <TrendingList movies={movieData} />;
+}
