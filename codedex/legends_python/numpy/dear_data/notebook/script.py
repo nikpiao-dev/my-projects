af4daf0d0d9bc19136168249fd4669a5d2dfb39f
Nik <nik.piao26@gmail.com>
2025-06-30 01:33:40 -0400

    added new file

---
+++ codedex/legends_python/numpy/dear_data/notebook/script.py
@@ -0,0 +1,27 @@
+"""
+
+
+Instructions
+Ahh that new book smell... we got a fresh new notebook!
+
+In the input box at the bottom, type:
+
+print('So it\'s not gonna be easy. It\'s gonna be really hard.')
+
+Press shift + enter to run the code. Did the output appear?
+
+Next, type the same thing again. This time, press just enter.
+
+It created a new line instead! Type this on the next line:
+
+print('We\'re gonna have to work at this everyday, but I want to do that because I want all the data, forever. You and me.')
+
+Press shift + enter to run the code block.
+
+"""
+
+import numpy as np
+
+print('So it\'s not gonna be easy. It\'s gonna be really hard.')
+
+print('We\'re gonna have to work at this everyday, but I want to do that because I want all the data, forever. You and me.')
