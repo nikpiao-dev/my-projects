d5a837a727cd3ff7799d1d649ce52dc7bd5ab04f
Nik <nik.piao26@gmail.com>
2025-06-30 01:40:57 -0400

    added new python file and updated the notes.txt file

---
+++ codedex/legends_python/python/hello_world/initials.py
@@ -0,0 +1,32 @@
+"""
+Instructions
+Create an initials.py program that displays your initials in block letters.
+
+First, start the program with a comment that says a fun fact about yourself.
+
+Then, create your block letters with your initials.
+
+For example, if your name is Dua Lipa, your initials would be D and L, and your block letters would look like this:
+
+DDDD   L
+D   D  L
+D   D  L
+D   D  L
+D   D  L
+D   D  L
+DDDD   LLLLL
+
+"""
+
+
+# #printing out a message
+# print('Hi')
+# print('Hi')   # I'm a comment, too!
+
+print("DDDD    L")
+print("D   D   L")
+print("D   D   L")
+print("D   D   L")
+print("D   D   L")
+print("D   D   L")
+print("DDDD    LLLLL")
