65b62e523de9a5a1c88c5c73887e907ba7e662c2
Nik <nik.piao26@gmail.com>
2025-06-30 01:42:09 -0400

    created new file for final activity in lesson hello_world

---
+++ codedex/legends_python/python/hello_world/blank.py
@@ -0,0 +1,52 @@
+"""
+Instructions
+Create a blank letter.py program.
+
+In this exercise, you will write a letter to your future self... in Python.
+
+
+
+Take a moment and think about what you hope to achieve on this journey.
+
+Use print() to output:
+
+Today's date.
+How you are feeling right now.
+What you want to accomplish by learning to code.
+A message to your older, wiser, and programmer self.
+Your favorite emoji to spice things up!
+
+"""
+
+print("March 15, 2025")
+print("I'm feeling alright.")
+print("I want to get better at Python")
+
+print("Make sure to stay healthy and practice fundamentals more")
+print("😊")
+
+import this
+
+"""
+
+Beautiful is better than ugly.
+Explicit is better than implicit.
+Simple is better than complex.
+Complex is better than complicated.
+Flat is better than nested.
+Sparse is better than dense.
+Readability counts.
+Special cases aren't special enough to break the rules.
+Although practicality beats purity.
+Errors should never pass silently.
+Unless explicitly silenced.
+In the face of ambiguity, refuse the temptation to guess.
+There should be one-- and preferably only one --obvious way to do it.
+Although that way may not be obvious at first unless you're Dutch.
+Now is better than never.
+Although never is often better than *right* now.
+If the implementation is hard to explain, it's a bad idea.
+If the implementation is easy to explain, it may be a good idea.
+Namespaces are one honking great idea -- let's do more of those!
+
+"""
