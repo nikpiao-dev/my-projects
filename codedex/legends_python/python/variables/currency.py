0e986c7a5f300a63110276bdacf524653a7d088d
Nik <nik.piao26@gmail.com>
2025-07-01 16:14:06 -0400

    added a new content in the lesson

---
+++ codedex/legends_python/python/variables/currency.py
@@ -0,0 +1,27 @@
+"""
+Instructions
+We just got home from a fun trip to South America, specifically Colombia, Peru, and Brazil. There's some leftover cash in:
+
+🇨🇴 Colombian pesos
+🇵🇪 Peruvian soles
+🇧🇷 Brazilian reais
+Create a currency.py program that asks the user for the amount they have in pesos, soles, and reais and calculates the total in USD.
+
+Make sure to Google the current exchange rates for pesos, soles, and reais!
+
+
+
+The output should look something like this, but with slightly different results:
+
+What do you have left in pesos? 5600
+What do you have left in soles? 105
+What do you have left in reais? 280
+413.0
+
+The sequence should be:
+
+Currency code example
+
+Cha-ching! Now you have the total in USD. 💰
+
+"""
