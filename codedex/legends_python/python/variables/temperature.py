84b6aa324fac76671f5e198865af2135f731c91f
Nik <nik.piao26@gmail.com>
2025-07-01 16:07:23 -0400

    added a new content in the python file

---
+++ codedex/legends_python/python/variables/temperature.py
@@ -0,0 +1,18 @@
+"""
+Instructions
+Create a temperature.py program that converts a number from Fahrenheit (°F) to Celsius (°C).
+
+Google the current temperature of Brooklyn, NY (where Codédex is based) in °F.
+
+Use the following formula and write it out in Python:
+
+
+Celsius= 
+1.8
+(Fahrenheit−32)
+​
+ 
+
+Print out the converted temperature. 🌡️
+
+"""
