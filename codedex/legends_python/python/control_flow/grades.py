44e43bb5419a7df46ea07a466d4d9d3abce11552
Nik <nik.piao26@gmail.com>
2025-07-02 17:32:16 -0400

    added new content in the grades.py file

---
+++ codedex/legends_python/python/control_flow/grades.py
@@ -0,0 +1,25 @@
+"""
+Instructions
+Holy moly, because the class's average was super low on the test, the teacher just added a curve for the test grades! 😭
+
+Create a grades.py program that checks whether a grade is above or below 55.
+
+Start by creating a variable called grade and give it a value between 0-100.
+
+Write a if/else statement for the following:
+
+If grade is greater than or equal to 55, then print "You passed."
+Else, print "You failed."
+After you run the code, change grade's value and rerun it. Do this a few times to make sure it's working as intended.
+"""
+
+import random
+
+grade = random.randint(40, 80)
+
+if grade > 55:
+  print('You passed!')
+  print('Your grade is:', grade)
+else:
+  print('You failed.')
+  print('Your grade is:', grade)
