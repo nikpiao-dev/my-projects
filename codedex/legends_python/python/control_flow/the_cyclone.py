651886cee8f7bb09257400164b7b8ece034fa3fa
Nik <nik.piao26@gmail.com>
2025-07-04 03:00:10 -0400

    added new content in the the_cyclone python file

--- codedex/legends_python/python/control_flow/the_cyclone.py
+++ codedex/legends_python/python/control_flow/the_cyclone.py
@@ -15,3 +15,25 @@ Else if they have enough credits, but they aren't tall enough, print "You are no
 Else if they are tall enough, but they don't have enough credits, print "You don't have enough credits."
 Else, print a message saying they have not met either requirement.
 """
+
+
+min_height = 137
+min_credits = 10
+
+user_height = int(input("What is your height in cm?: "))
+user_credits = int(input("How many credits do you have?: "))
+min_height = 137
+min_credits = 10
+
+user_height = int(input("What is your height in cm?: "))
+user_credits = int(input("How many credits do you have?: "))
+
+if user_height >= min_height and user_credits >= min_credits:
+    print('Enjoy the ride!')
+elif user_credits >= min_credits and user_height < min_height:
+    print('You are not tall enough to ride.')
+elif user_height >= min_height and user_credits < min_credits:
+    print("You don't have enough credits.")
+else:
+    print('You are not eligible to go on this ride. Not tall enough and do not have the minimum number of credits needed.')
+
