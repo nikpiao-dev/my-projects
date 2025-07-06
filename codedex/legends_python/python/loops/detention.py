23f37ec61b9649e214f127e8da187615e4948e3c
Nik <nik.piao26@gmail.com>
2025-07-06 15:13:02 -0400

    added two new files in the loops folder

---
+++ codedex/legends_python/python/loops/detention.py
@@ -0,0 +1,12 @@
+"""
+Instructions
+detention
+
+Suppose you got detention and the teacher wants you to write "I will not use Snapchat in class" on the whiteboard 100 times.
+
+Create a detention.py program that does this using code.
+
+(This is where we begin to see the true power of computing. What takes humans hours can take a computer microseconds. 🤯)
+
+"""
+
