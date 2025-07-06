71186780f46d5b1d2957ff0ab046e94f093f871b
Nik <nik.piao26@gmail.com>
2025-07-06 15:11:17 -0400

    added new content in the guess python file

--- codedex/legends_python/python/loops/guess.py
+++ codedex/legends_python/python/loops/guess.py
@@ -20,3 +20,16 @@ First, introduce a variable called tries at the top and give it a value of 0.
 Then, add a second condition with the tries variable to the while loop using a relational operator.
 
 """
+
+# Exercise:
+
+
+guess = 0
+tries = 0
+
+while guess != 6:
+  guess = int(input("Guess the number:  "))
+  tries += 1
+
+print("You got it!")
+print(f"It took you {tries} tries to guess the correct number({guess}).")
