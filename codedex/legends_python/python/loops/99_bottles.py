4eef35b1ed0c2afc952e36caa10884d2710a6108
Nik <nik.piao26@gmail.com>
2025-07-07 20:46:14 -0400

    added new content in bottle python file

--- codedex/legends_python/python/loops/99_bottles.py
+++ codedex/legends_python/python/loops/99_bottles.py
@@ -15,3 +15,9 @@ Take one down, pass it around
 And don't forget to use string interpolation!
 """
 
+
+for i in range(99, 0 , -1):
+  print(f'{i} bottles of beer on the wall')
+  print(f'{i} bottles of beer')
+  print('Take one down, pass it around')
+  print(f'{i-1} bottles of beer on the wall')
