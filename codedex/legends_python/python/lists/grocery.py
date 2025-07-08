760d0ee12be3fdcce51c7d2c255032a378bfb268
Nik <nik.piao26@gmail.com>
2025-07-08 18:50:46 -0400

    added new instructions in grocery python file

---
+++ codedex/legends_python/python/lists/grocery.py
@@ -0,0 +1,14 @@
+"""
+Instructions
+Create a grocery.py program with a grocery list of things that you need from the store:
+
+'🥚 Eggs'
+'🥑 Avocados'
+'🍪 Cookies'
+'🌶 Hot Pepper Jam'
+'🫐 Blueberries'
+'🥦 Broccoli'
+Print out grocery to make sure you got it!
+
+"""
+
