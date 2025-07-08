f0fa040de9fbb5c5dc404396527c83b7198fc52a
Nik <nik.piao26@gmail.com>
2025-07-07 20:45:44 -0400

    added new instruction in the bottle.py file

---
+++ codedex/legends_python/python/loops/99_bottles.py
@@ -0,0 +1,17 @@
+"""
+Instructions
+"99 Bottles of Beer" is an old song that annoying kids, oops I mean everyone, sang on road trips to pass the time.
+
+Create a 99_bottles.py program and use a for loop and a range() function to print out all the verses of "99 Bottles of Beer."
+
+99 bottles of beer on the wall
+
+99 bottles of beer
+
+Take one down, pass it around
+
+98 bottles of beer on the wall
+
+And don't forget to use string interpolation!
+"""
+
