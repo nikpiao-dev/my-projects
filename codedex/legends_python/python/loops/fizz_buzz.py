ba6c695ed408fa471974e0b67c036f42295751ce
Nik <nik.piao26@gmail.com>
2025-07-07 20:47:43 -0400

    added new files inthe loops folder

---
+++ codedex/legends_python/python/loops/fizz_buzz.py
@@ -0,0 +1,34 @@
+"""
+Instructions
+Fizz Buzz is a children's word game that teaches division. It's also a classic technical interview question at countless companies. 🐝
+
+
+
+Though this challenge may appear simple to experienced coders, it is designed to weed out 90% of job candidates who cannot apply their coding knowledge to a new problem creatively. Want to give it a try?
+
+Create a fizz_buzz.py program that outputs numbers from 1 to 100.
+
+Here's the catch:
+
+For multiples of 3, print "Fizz" instead of the number.
+For multiples of 5, print "Buzz" instead of the number.
+Here's the tricky part: For multiples of 3 and 5, print "FizzBuzz".
+The output should look like:
+
+1
+2
+Fizz
+4
+Buzz
+Fizz
+7
+8
+Fizz
+Buzz
+11
+Fizz
+13
+14
+FizzBuzz
+...
+"""
