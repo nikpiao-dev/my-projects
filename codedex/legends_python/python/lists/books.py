7dee221ed44d592f35670825ebeee86a54067b0f
Nik <nik.piao26@gmail.com>
2025-07-09 20:57:43 -0400

    added new content

--- codedex/legends_python/python/lists/books.py
+++ codedex/legends_python/python/lists/books.py
@@ -14,5 +14,23 @@ Suppose we want to add 'Pachinko' to the list. Can you use a list method to do s
 Let's say we finished reading 'The Fault in Our Stars' and '1984'. Can you use the .remove() method to remove one and the .pop() method to remove the other?
 
 Print the updated list out to make sure everything's good to go!
+
 """
+
+
+books = ['Harry Potter', '1984', 'The Fault in Our Stars', 'The Mom Test', 'Life in Code']
+
+
+print("Original list:", books)
+print()
+
+# add pachinko
+
+books.append("Pachinko")
+books.remove('The Fault in Our Stars')
+books.pop(1)
+
+
+print("Updated list:", books)
+print()
 
