0ac8653a76c4c13df14445e6665ccb9344961cea
Nik <nik.piao26@gmail.com>
2025-07-10 21:13:50 -0400

    added new instructions in the python file

---
+++ codedex/legends_python/python/lists/mixtape.py
@@ -0,0 +1,24 @@
+"""
+Instructions
+Back in the good ol' days, a mixtape is a compilation of music, a playlist, recorded onto a cassette tape or a CD.
+
+Create a mixtape.py program and think of a theme for the songs.
+
+Make a list called playlist and throw in some songs.
+
+For example:
+
+# 💿 Theme: Indie Travel Songs
+
+playlist = [
+  'Porches - rangerover',
+  'Mount Eerie - You Swan, Go On',
+  'Carolyn Polachek - Look at Me Now',
+  'Pinegrove - Darkness',
+  'LVL UP - Spirit Was',
+  'Mitski - First Love / Late Spring'
+]
+
+Now loop over the list and print everything out! 📻
+
+"""
