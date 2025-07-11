d340da21e40321cf8f0e667141f989d42e13457f
Nik <nik.piao26@gmail.com>
2025-07-10 21:16:01 -0400

    added new files and content

---
+++ codedex/legends_python/python/lists/bucket_list.py
@@ -0,0 +1,29 @@
+
+"""
+Instructions
+A bucket list is a number of experiences or achievements that a person hopes to have or accomplish during their lifetime. 🪣
+
+
+
+Create a bucket_list.py program with your own unique bucket list.
+
+First, create a things_to_do list, and add things you want to do in your lifetime.
+
+For example:
+
+things_to_do = [
+  '🚀 Create the dopest learn to code platform ever.',
+  '⛰️ Hike the Pacific Crest Trail.',
+  '🏡 Build an A-frame house and raise some goats.',
+  '🌏 Live somewhere in Asia for a year.',
+  '🎸 Release an album.',
+  '📝 Write a book.',
+  '🏆 Reach 100k subscribers on YouTube.',
+  '🚐 Road trip with the fam.',
+  '🍳 Open a cozy diner upstate.',
+  '👴🏻 Grow old with no regrets.'
+]
+
+Now, iterate over the list and print everything out.
+
+"""
