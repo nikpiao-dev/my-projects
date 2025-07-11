c12e88af4f6e885236fd6a73c957fca9dc9170aa
Nik <nik.piao26@gmail.com>
2025-07-10 21:14:20 -0400

    added new content

--- codedex/legends_python/python/lists/mixtape.py
+++ codedex/legends_python/python/lists/mixtape.py
@@ -22,3 +22,19 @@ playlist = [
 Now loop over the list and print everything out! 📻
 
 """
+
+# 💿 Theme: Indie Travel Songs
+
+playlist = [
+  'Porches - rangerover',
+  'Mount Eerie - You Swan, Go On',
+  'Carolyn Polachek - Look at Me Now',
+  'Pinegrove - Darkness',
+  'LVL UP - Spirit Was',
+  'Mitski - First Love / Late Spring'
+]
+
+for song in playlist:
+  print(song)
+
+
