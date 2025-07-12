d3d729d18ece40448d9fd77160a13d72eaa644cd
Nik <nik.piao26@gmail.com>
2025-07-11 21:32:02 -0400

    added new content in the python file

--- codedex/legends_python/python/functions/fortune_cookie.py
+++ codedex/legends_python/python/functions/fortune_cookie.py
@@ -21,3 +21,47 @@ Then, call the fortune() function three times and see what you get!
 Bonus: If you're daring, rewrite the function without an if/elif/else.
 
 """
+
+
+import random
+
+# def fortune():
+#   random_fortune = random.randint(0, 8)
+  
+#   if random_fortune == 1:
+#     return "Don't pursue happiness – create it"
+#   elif random_fortune == 2:
+#     return "All things are difficult before they are easy"
+#   elif random_fortune == 3:
+#     return "The early bird gets the worm, but the second mouse gets the cheese"
+#   elif random_fortune == 4:
+#     return "Someone in your life needs a letter from you."
+#   elif random_fortune == 5:
+#     return "Don't just think. Act"
+#   elif random_fortune == 6:
+#     return "Your heart will skip a beat."
+#   elif random_fortune == 7:
+#     return "The fortune you search for is in another cookie"
+#   elif random_fortune == 8:
+#     return "Help! I'm being held prisoner in a Chinese bakery!"
+#   else:
+#     return "Sorry, no fortune for you today."
+
+options = [
+  'Don’t pursue happiness – create it.',
+  'All things are difficult before they are easy.',
+  'The early bird gets the worm, but the second mouse gets the cheese.',
+  'If you eat something and nobody sees you eat it, it has no calories.',
+  'Someone in your life needs a letter from you.',
+  'Don’t just think. Act!',
+  'Your heart will skip a beat.',
+  'The fortune you search for is in another cookie.',
+  'Help! I’m being held prisoner in a Chinese bakery!'
+]
+def fortune():
+  random_fortune = random.randint(0, len(options) - 1)
+  print(options[random_fortune])
+
+fortune()
+fortune()
+fortune()
