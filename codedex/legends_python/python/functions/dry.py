2a4fa5219e582d378b2a8bbb8a8bc2618741ccec
Nik <nik.piao26@gmail.com>
2025-07-11 21:29:20 -0400

    added new content in the py file

--- codedex/legends_python/python/functions/dry.py
+++ codedex/legends_python/python/functions/dry.py
@@ -11,3 +11,22 @@ For the new function, try to read the documentation (😵‍💫) or Google it (
 Add a comment for each built-in function to explain how they work.
 
 """
+
+import random
+
+list_of_foods = ['celery', 'broccoli', 'cabbage']
+
+# This was the first function introduced in the course
+print('Hello, World!')
+
+# This function calculates the maximum of two numbers a and b
+print(max(3, 5))
+
+# This function calculates the minimum of two numbers a and b
+print(min(-1, 7))
+
+# This function calculates the length of a list
+print(len(list_of_foods))
+
+# This function calculates a to the power b
+print(pow(2, 6))
