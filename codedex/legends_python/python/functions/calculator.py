4c1f2d2d55943fc9e0a3b0d623a428a5578adbd1
Nik <nik.piao26@gmail.com>
2025-07-13 22:40:53 -0400

    added new content

--- codedex/legends_python/python/functions/calculator.py
+++ codedex/legends_python/python/functions/calculator.py
@@ -14,3 +14,31 @@ Make sure to return the result in each function definition.
 Test your calculator by calling each function once to make sure that it works!
 
 """
+
+
+def add(x, y):
+  return x + y
+
+def subtract(x, y):
+  return x - y
+
+def multiply( x, y):
+  return x * y
+
+def divide(x, y):
+  return x / y
+
+def exp(x, y):
+  return x ** y
+
+print(add(1, 2))
+print(subtract(3, 4))
+print(multiply(5, 6))
+print(divide(7, 8))
+print(exp(9, 10))
+
+print(add(3, 5))
+print(subtract(7, 2))
+print(multiply(4, 8))
+print(divide(9, 3))
+print(exp(2, 3))
