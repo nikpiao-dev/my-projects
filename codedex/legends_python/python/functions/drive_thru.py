951dd11eb47e70cd2230a47ee8612177ae0c7ee5
Nik <nik.piao26@gmail.com>
2025-07-16 19:36:17 -0400

    added new stuff

---
+++ codedex/legends_python/python/functions/drive_thru.py
@@ -0,0 +1,23 @@
+"""
+Instructions
+When you go to a drive-thru like McDonald's, you can order food using the item numbers. For example, a Happy Meal might be a #3!
+
+Create a drive_thru.py program with your favorite fast food chain's menu.
+
+Define a get_item() function that takes in one parameter, the number of the item you want to order, and returns the name of that item!
+
+For example, if you called the function with:
+
+Argument value 1, it could return '🍔 Cheeseburger'.
+Argument value 2, it could return '🍟 Fries'.
+Argument value 3, it could return '🥤 Soda'.
+Argument value 4, it could return '🍦 Ice Cream'.
+Argument value 5, it could return '🍪 Cookie'.
+Make sure to call this function a few times to make sure that it works!
+
+Lastly, let's do the following:
+
+Create a welcome menu and put that in a welcome() function.
+Create a main program that takes in user input with input().
+
+"""
