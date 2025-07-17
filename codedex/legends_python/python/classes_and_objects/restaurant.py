343865f9fe4d12a54ead10b43bf71846553d70ed
Nik <nik.piao26@gmail.com>
2025-07-17 17:59:29 -0400

    added new python file

---
+++ codedex/legends_python/python/classes_and_objects/restaurant.py
@@ -0,0 +1,15 @@
+"""
+Instructions
+
+
+Suppose we're building a food delivery app and want to store restaurants' info. 🍽️
+
+Create a Restaurant class in a restaurants.py file with the following attributes:
+
+.name of the type str
+.category of the type str
+.rating of the type float
+.delivery of the type bool
+Make these empty/default values for now.
+
+"""
