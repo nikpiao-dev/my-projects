ebc3d4256a6b130274db4e3998a6510d866d1941
Nik <nik.piao26@gmail.com>
2025-07-17 18:02:07 -0400

    added new content

--- codedex/legends_python/python/classes_and_objects/bobs_burgers.py
+++ codedex/legends_python/python/classes_and_objects/bobs_burgers.py
@@ -13,3 +13,38 @@ Once you do that, create two more instances of the Restaurant class with your fa
 Then, use print(vars()) to output each of the three restaurants!
 
 """
+
+
+class Restaurant:
+  name = ''
+  category = ''
+  rating = 0.0
+  delivery = True
+
+bobs_burgers = Restaurant()
+sunny_sushi = Restaurant()
+pasta_palace = Restaurant()
+
+# first instance
+bobs_burgers.name = "Bob's Burgers"
+bobs_burgers.category = "American Diner"
+bobs_burgers.rating = 4.7
+bobs_burgers.deliivery = False
+
+# second instance
+sunny_sushi.name = "Sunny Sushi"
+sunny_sushi.category = "Japanese"
+sunny_sushi.rating = 4.5
+sunny_sushi.delivery = True
+
+# third instance
+pasta_palace.name = "Pasta Palace"
+pasta_palace.category = "Italian"
+pasta_palace.rating = 4.9
+pasta_palace.delivery = False
+
+
+print(vars(bobs_burgers))
+print(vars(sunny_sushi))
+print(vars(pasta_palace))
+
