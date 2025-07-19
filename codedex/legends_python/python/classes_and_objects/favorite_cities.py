4383ae059c8dbd4635c991a7f0f26435a5038693
Nik <nik.piao26@gmail.com>
2025-07-18 21:00:18 -0400

    added new content in python file

--- codedex/legends_python/python/classes_and_objects/favorite_cities.py
+++ codedex/legends_python/python/classes_and_objects/favorite_cities.py
@@ -15,7 +15,53 @@ Next, create an object for your hometown and assign the attributes above.
 Lastly, create another object for the city that you've always wanted to visit!
 
 Bonus: Add 2-3 more attributes, like nickname, founding year, mayor, etc.
-
 """
+
+
+
+class City:
+    def __init__(self, name, country, population, landmarks, founding_year, mayor):
+        self.name = name
+        self.country = country
+        self.population = population
+        self.landmarks = landmarks
+        self.founding_year = founding_year
+        self.mayor = mayor
+
+# Create City objects
+tampa = City(
+    name="Tampa",
+    country="USA",
+    population=395000,
+    landmarks=["Tampa Riverwalk", "Busch Gardens", "Ybor City"],
+    founding_year=1823,
+    mayor="Jane Castor"
+)
+
+seattle = City(
+    name="Seattle",
+    country="USA",
+    population=737000,
+    landmarks=["Space Needle", "Pike Place Market", "Chihuly Garden and Glass"],
+    founding_year=1851,
+    mayor="Bruce Harrell"
+)
+
+new_york = City(
+    name="New York",
+    country="USA",
+    population=8419000,
+    landmarks=["Statue of Liberty", "Central Park", "Empire State Building"],
+    founding_year=1624,
+    mayor="Eric Adams"
+)
+
+print(vars(tampa))
+print()
+print(vars(seattle))
+print()
+print(vars(new_york))
+print()
+
 
 
