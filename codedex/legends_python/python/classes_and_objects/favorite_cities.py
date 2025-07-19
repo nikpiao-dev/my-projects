f0aecbf430cde036d6eff8a33faf95de8abe9ffc
Nik <nik.piao26@gmail.com>
2025-07-18 20:59:22 -0400

    added new python file

---
+++ codedex/legends_python/python/classes_and_objects/favorite_cities.py
@@ -0,0 +1,21 @@
+"""
+Instructions
+Ever wonder how many people live in New York City? What about London?
+
+Create a favorite_cities.py program.
+
+Let's make a City class that uses the __init__() method to define the following attributes:
+
+name (string)
+country (string)
+population (integer rounded to the nearest thousand people)
+landmarks (list of strings)
+Next, create an object for your hometown and assign the attributes above.
+
+Lastly, create another object for the city that you've always wanted to visit!
+
+Bonus: Add 2-3 more attributes, like nickname, founding year, mayor, etc.
+
+"""
+
+
