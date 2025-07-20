c2a8787f328dc1fa64b774555e845e24c93c0b0c
Nik <nik.piao26@gmail.com>
2025-07-19 21:20:16 -0400

    added new content in the file

--- codedex/legends_python/python/classes_and_objects/bank_account.py
+++ codedex/legends_python/python/classes_and_objects/bank_account.py
@@ -24,3 +24,38 @@ Withdraw $25 from the account.
 Print the current account balance.
 
 """
+
+
+class BankAccount:
+    def __init__(self, first_name, last_name, account_id, account_type, pin, balance):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.account_id = account_id
+        self.account_type = account_type
+        self.pin = pin
+        self.balance = balance
+
+    def deposit(self, amount):
+        self.balance += amount
+        return f"Balance after deposit of ${amount}:  {self.balance}"
+  
+    def withdraw(self, amount):
+        if amount > self.balance:
+            return "Insufficient Funds"
+        self.balance -= amount
+        return f"Balance after withdrawal of ${amount}: {self.balance}"
+  
+    def display_balance(self):
+        return f"Current Balance: ${self.balance}"
+
+bank_account = BankAccount("Jerry", "Stone", 872143, "Checking", 9962, 3174.56)
+
+print(bank_account.display_balance())
+print()
+
+print(bank_account.deposit(100))
+print(bank_account.withdraw(50.92))
+
+print()
+print(bank_account.display_balance())
+
