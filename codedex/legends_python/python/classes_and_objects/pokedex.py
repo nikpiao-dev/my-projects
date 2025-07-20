2a7a1552a45817d06135253db618f272ea2d90c7
Nik <nik.piao26@gmail.com>
2025-07-20 19:44:53 -0400

    added new content in the python file

--- codedex/legends_python/python/classes_and_objects/pokedex.py
+++ codedex/legends_python/python/classes_and_objects/pokedex.py
@@ -32,3 +32,62 @@ Are you ready to earn the next badge?
 Bonus: For all the super fans, try and add more attributes to the Pokemon class definition, like level, region, height, or weight.
 
 """
+
+
+class Pokemon:
+  def __init__(self, entry, name, types, description, is_caught):
+    self.entry = entry
+    self.name = name
+    self.types = types
+    self.description = description
+    self.is_caught = is_caught
+
+  def speak(self, sound):
+    return sound * 2;
+
+  # def display_detail(self):
+  #    def display_details(self):
+  #   print('Entry Number: ' + str(self.entry))
+  #   print('Name: ' + self.name)
+
+  #   if len(self.types) == 1:
+  #     print('Type: ' + self.types[0])
+  #   else:
+  #     print('Type: ' + self.types[0] + '/' + self.types[1])
+    
+  #   print('Description: ' + self.description)
+
+  #   if self.is_caught:
+  #     print(self.name + ' has already been caught!')
+  #   else:
+  #     print(self.name + ' hasn\'t been caught yet.')
+
+  def display_details(self):
+    print("Entry number: " , self.entry)
+    print("Name: ", self.name)
+    print("Type: ", self.types)
+    print("Description: ", self.description)
+    if self.is_caught == True:
+      print(self.name + "has already been caught!")
+    else:
+      print(self.name + "has not been caught yet.")
+  
+
+bulbasaur = Pokemon(1, "Bulbasaur", ["Grass", "Poison"], "It has a plant bulb on its back that stores energy and grows as it absorbs sunlight. As it evolves, the bulb blooms into a large flower.", True)
+magnemite = Pokemon(81, "Magnemite", ["Electric", "Steel"], "It generates powerful electromagnetic fields, which can cause metal objects to be attracted to it.", False)
+sneasel = Pokemon(215, "Sneasel", ["Dark", "Ice"], "It’s known for its agility and is often found in icy, mountainous areas. Sneasel is skilled in sneaking around, and it uses its claws to climb and slash opponents.", True)
+
+print(bulbasaur.speak("bulba"))
+print()
+bulbasaur.display_details()
+print()
+
+print(magnemite.speak("Bzzzt!"))
+print()
+magnemite.display_details()
+print()
+
+print(sneasel.speak("Krii"))
+print()
+sneasel.display_details()
+
