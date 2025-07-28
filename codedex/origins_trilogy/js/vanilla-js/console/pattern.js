8b030fd1574108773c63381d7052f42c2c65bd5e
Nik <nik.piao26@gmail.com>
2025-07-28 18:24:35 -0400

    added new js file

---
+++ codedex/origins_trilogy/js/vanilla-js/console/pattern.js
@@ -0,0 +1,26 @@
+"""
+
+Instructions
+Suppose we want the output to look exactly like this pattern below:
+
+   a
+  b c
+ d e f
+g h i j
+   k
+
+How can you do that?
+
+Create a JavaScript program that prints this pattern exactly as shown.
+
+This will likely take some trial and error, but give it a shot!
+
+"""
+
+
+console.log("   a  ");
+console.log("  b c ");
+console.log(" d e f ");
+console.log("g h i j");
+console.log("   k  ");
+
