ee66ca2cf43099cd587a0a19097b51003a31a4f3
Nik <nik.piao26@gmail.com>
2025-07-28 18:23:41 -0400

    created a new python file

---
+++ codedex/origins_trilogy/js/vanilla-js/console/index.js
@@ -0,0 +1,14 @@
+"""
+Instructions
+With the editor on the right, use console.log() to output your favorite food! 🍎
+
+Were you able to log the message?
+
+Bonus: Try repeating these instructions in your browser's web console.
+
+"""
+
+console.log("👋 Howdy!");
+console.log('👋 Howdy!');
+
+console.log("Favorite food is Ice Cream!");
