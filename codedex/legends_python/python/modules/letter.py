52137cef62f8bf5a7ae47816f90a31f70539c573
Nik <nik.piao26@gmail.com>
2025-07-27 20:41:33 -0400

    created new py file

---
+++ codedex/legends_python/python/modules/letter.py
@@ -0,0 +1,31 @@
+"""
+
+Instructions
+Remember when you wrote a letter to yourself called letter.py in the very first chapter? We are giving it back to you! 💌
+
+It should appear in the code editor on the right. Do you recognize it? Take a moment and reflect on what you wrote yourself all those chapters ago!
+
+And that's not all.
+
+Open your terminal and type python3 to prompt it for Python code (or skip this step and do the following in our online editor).
+
+Then, run the following code for some eternal words of wisdom:
+
+import this
+
+"The Zen of Python" is a poem written by Tim Peters, a long-time Pythonista who wrote down Python's design style guide using 20 aphorisms. Only 19 are written down. Forty-two programs later, you can now decipher the wisdom:
+
+The Zen of Python, by Tim Peters
+
+Beautiful is better than ugly.
+Explicit is better than implicit.
+Simple is better than complex.
+Complex is better than complicated.
+...
+
+The end is just the beginning!
+
+This is where things get exciting, now that you have all the foundational building blocks. The world of programming just opened up in front of you officially.
+
+
+"""
