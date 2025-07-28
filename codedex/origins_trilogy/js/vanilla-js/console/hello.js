f375b4c7d36df7fc0d37866535fabca1ad280e59
Nik <nik.piao26@gmail.com>
2025-07-27 20:45:08 -0400

    added new file and content

---
+++ codedex/origins_trilogy/js/vanilla-js/console/hello.js
@@ -0,0 +1,18 @@
+"""
+Instructions
+Copy and paste the following onto line 3 of the code editor on the right.
+
+console.log("Hello internet!");
+
+Press the "Run" button and wait 1-2 seconds.
+
+This should appear in the Console window if you did it correctly:
+
+Hello internet
+
+You are now ready for the journey ahead. ( ⸝⸝•ᴗ•⸝⸝ )੭⁾⁾
+
+"""
+
+
+console.log("Hello internet!");
