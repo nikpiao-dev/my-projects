761300a71a4fb6a535b559d79e8ca1e0abedbd17
Nik <nik.piao26@gmail.com>
2025-07-29 19:46:59 -0400

    created a new file

---
+++ codedex/origins_trilogy/js/vanilla-js/console/receipt.js
@@ -0,0 +1,47 @@
+"""
+Instructions
+You are going to recreate a receipt of a purchase from the last store or restaurant that you went to... using JavaScript! 🧾
+
+Make sure to check your pockets and wallet if you forgot.
+
+
+
+Use console.log() methods to print the following info:
+
+Name of the store/restaurant.
+A row that says "Order Details".
+A row with column names for "Item", "Quantity", and "Price".
+At least two items that were purchased.
+The total price for the purchase.
+The output should look like:
+
+==================================
+         McDONALD'S RECEIPT       
+----------------------------------
+           Order Details          
+----------------------------------
+Item           Quantity   Price
+----------------------------------
+🍔 Burger       1          2.99   
+🍟 Fries        1          2.49   
+🥤 Soda         1          1.99   
+----------------------------------
+Total                      7.47   
+==================================
+
+"""
+
+
+console.log("==============================");
+console.log("       WENDY'S RECEIPT        ");
+console.log("------------------------------");
+console.log("       Order Details          ");
+console.log("------------------------------");
+console.log("Item       Quantity    Price  ");
+console.log("------------------------------");
+console.log("Dave's Single 1        4.99   ");
+console.log("Fries         1        2.79   ");
+console.log("Drink         1        2.19   ");
+console.log("------------------------------");
+console.log("Total                  9.97   ");
+console.log("==============================");
