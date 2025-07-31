6e4108d6c4d116012525710903f74eb59962734d
Nik <nik.piao26@gmail.com>
2025-07-31 12:34:08 -0400

    added new content

---
+++ codedex/origins_trilogy/js/vanilla-js/variables/data-type.js
@@ -0,0 +1,22 @@
+"""
+Instructions
+Create some variables for your favorite company with the following data types:
+
+companyName, a string.
+foundingYear, a number.
+isActive, a Boolean.
+fundingAmount, undefined.
+Print them out using console.log().
+
+"""
+
+
+let companyName = "Horshey Corps";
+let foundingYear = 2025;
+let isActive = false;
+let fundingAmount;
+
+console.log(companyName);
+console.log(foundingYear);
+console.log(isActive);
+console.log(fundingAmount);
