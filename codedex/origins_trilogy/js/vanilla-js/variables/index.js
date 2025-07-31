f2cdd5fbb0addba3f4e06367aed8129c0920a57a
Nik <nik.piao26@gmail.com>
2025-07-31 12:32:57 -0400

    added new js file

---
+++ codedex/origins_trilogy/js/vanilla-js/variables/index.js
@@ -0,0 +1,27 @@
+"""
+Instructions
+Create 4 variables for building a user profile on your favorite website:
+
+Two const variables for your first name and favorite color.
+Two let variables for your current location and mood.
+Lastly, print them out with console.log().
+
+Reassign a new value to at least one of the let variables and print it one more time with console.log().
+
+
+"""
+
+
+const firstName = "Nick";
+const favColor = "Blue";
+
+let currLocation = "USA";
+let mood = "Neutral";
+
+console.log(firstName);
+console.log(favColor);
+console.log(currLocation);
+console.log(mood);
+
+mood = "Hopeful";
+console.log(mood);
