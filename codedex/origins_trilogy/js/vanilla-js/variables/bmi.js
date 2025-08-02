c224467b0503abd374ef1a1871674d5d1787a2c1
Nik <nik.piao26@gmail.com>
2025-08-02 07:57:22 -0400

    created a new js file

---
+++ codedex/origins_trilogy/js/vanilla-js/variables/bmi.js
@@ -0,0 +1,20 @@
+"""
+Instructions
+The body mass index (BMI) was created by a Belgian mathematician in the 1850s and it's used by health and nutrition professionals to estimate human body fat in certain populations.
+
+It is computed by taking an individual's weight (mass) in kilograms and dividing it by the square of their height in meters.
+
+
+bmi= 
+height 
+2
+ 
+mass
+​
+ 
+
+Create a program that calculates your own BMI.
+
+Author's note: Psst. BMI is an archaic and oversimplified way to measure personal health. It was created by a mathematician — not a doctor! 💡
+
+"""
