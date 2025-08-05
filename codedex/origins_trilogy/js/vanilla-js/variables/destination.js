cedca79d2ade4ab89bf5af31406d568df4750620
Nik <nik.piao26@gmail.com>
2025-08-04 20:15:34 -0400

    created new js file

---
+++ codedex/origins_trilogy/js/vanilla-js/variables/destination.js
@@ -0,0 +1,22 @@
+"""
+
+Instructions
+The year is 2099... we can now take a vacation on the moon or Mars! 🚀
+
+
+
+Create a weight conversion program that:
+
+Takes your weight on Earth.
+Converts it to your weight on either the moon or Mars.
+To calculate your weight on Mars:
+
+
+destination_weight=earth_weight×0.38
+
+The output of the program should look like this:
+
+Your weight on Earth is 210 pounds.
+Your weight on Mars is 79.8 pounds.
+
+"""
