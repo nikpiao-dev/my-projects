4467b31e58491b8fb6799f84005bbe9344e5ab5a
Nik <nik.piao26@gmail.com>
2025-08-05 20:23:22 -0400

    created new js file for if statements

---
+++ codedex/origins_trilogy/js/vanilla-js/conditionals/good-morning.js
@@ -0,0 +1,20 @@
+"""
+
+Instructions
+Create a good-morning.js program that prints a message if it's early in the day.
+
+Define an hour variable and give it the current time of day.
+
+Write an if statement for the following:
+
+If hour < 12, print “Good morning 🌞” and some of your morning routines.
+After you run the code, change hour's value and run it again. Do this a few times to make sure the program is working as intended.
+
+"""
+
+let hour = 2;
+
+if (hour < 12) {
+  console.log('Good Morning 🌞');
+} 
+
