02788e7553dfdc69022f40be0c55bdbe3f600905
Nik <nik.piao26@gmail.com>
2025-08-05 20:20:52 -0400

    made new js file

---
+++ codedex/origins_trilogy/js/vanilla-js/conditionals/coin.js
@@ -0,0 +1,25 @@
+"""
+Instructions
+Before we dive into how conditional statements are made in the next exercise, let's do a demo using a coin flip!
+
+Create a coin.js program and type in the following:
+
+let num = Math.random();
+
+if (num > 0.5) {
+  console.log("Heads");
+} else {
+  console.log("Tails");
+}
+
+Note: We will learn about Math.random() later in the chapter.
+
+All you need to know is that this program simulates a coin toss:
+
+≈ 50% of the time, it's "Heads".
+≈ 50% of the time, it's "Tails".
+Run the program 5 times to get a taste of the conditionals!
+
+How many times did we get Heads? How many for Tails?
+
+"""
