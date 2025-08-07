5d15905f1b140065fd73b4e035fa473e9f8a0270
Nik <nik.piao26@gmail.com>
2025-08-07 19:13:22 -0400

    made changes in js file

--- codedex/origins_trilogy/js/vanilla-js/conditionals/good-morning.js
+++ codedex/origins_trilogy/js/vanilla-js/conditionals/good-morning.js
@@ -1,5 +1,5 @@
 """
-
+-- OLD:
 Instructions
 Create a good-morning.js program that prints a message if it's early in the day.
 
@@ -10,7 +10,19 @@ Write an if statement for the following:
 If hour < 12, print “Good morning 🌞” and some of your morning routines.
 After you run the code, change hour's value and run it again. Do this a few times to make sure the program is working as intended.
 
+
+
+-- NEW:
+
+Go back to your if statement from the previous exercise and turn it into an if/else for the following:
+
+If hour < 12, then print “Good morning 🌞” and some of your morning routines.
+Else, print “Good afternoon ☁️” and some of your afternoon rituals.
+After you run the code, change hour's value and run it again. Do this a few times to make sure the program is working as intended.
+
 """
+
+// Old 
 
 let hour = 2;
 
@@ -18,3 +30,14 @@ if (hour < 12) {
   console.log('Good Morning 🌞');
 } 
 
+
+
+// NEW
+
+let hour = 12;
+
+if (hour < 12) {
+  console.log('Good morning 🌞')
+} else {
+  console.log('Good afternoon ☁️')
+}
