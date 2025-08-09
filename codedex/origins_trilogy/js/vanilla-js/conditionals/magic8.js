1aa44e661fb05d399251f08ff12e405ea5223384
Nik <nik.piao26@gmail.com>
2025-08-09 19:21:21 -0400

    added new file

---
+++ codedex/origins_trilogy/js/vanilla-js/conditionals/magic8.js
@@ -0,0 +1,29 @@
+"""
+
+Instructions
+The Magic 8 Ball is a popular office toy and children's toy invented in the 1940's for fortune-telling and advice seeking. 🎱
+
+It's an oversized 8 ball with some of the following answers:
+
+Yes - definitely.
+It is decidedly so.
+Without a doubt.
+Reply hazy, try again.
+Ask again later.
+Better not tell you now.
+My sources say no.
+Outlook not so good.
+Very doubtful.
+Create a magic8.js program that can respond to any Yes or No questions with a different answer each time it executes.
+
+The output should have the following format:
+
+Question:       [Question]
+Magic 8 Ball:   [Answer]
+
+For example:
+
+Question:       Is Codédex better than Udemy yet?
+Magic 8 Ball:   Better not tell you now.
+
+"""
