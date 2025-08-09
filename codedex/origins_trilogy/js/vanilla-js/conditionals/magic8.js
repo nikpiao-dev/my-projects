fbd89ed7a544877bec36ccc66bf5b49ec7d86738
Nik <nik.piao26@gmail.com>
2025-08-09 19:21:52 -0400

    added new content in js file

--- codedex/origins_trilogy/js/vanilla-js/conditionals/magic8.js
+++ codedex/origins_trilogy/js/vanilla-js/conditionals/magic8.js
@@ -27,3 +27,34 @@ Question:       Is Codédex better than Udemy yet?
 Magic 8 Ball:   Better not tell you now.
 
 """
+
+
+
+let question = "Will I find a hidden treasure this year?";
+let randomNumber = Math.floor(Math.random() * 10);
+let answer;
+
+if (randomNumber === 1) {
+  answer = 'Yes - definitely';
+} else if (randomNumber === 2) {
+  answer = 'It is decidedly so';
+} else if (randomNumber === 3) {
+  answer = 'Without a doubt';
+} else if (randomNumber === 4) {
+  answer = 'Reply hazy, try again';
+} else if (randomNumber === 5) {
+  answer = 'Ask again later';
+} else if (randomNumber === 6) {
+  answer = 'Better not tell you now';
+} else if (randomNumber === 7) {
+  answer = 'My sources say no';
+} else if (randomNumber === 8) {
+  answer = 'Outlook not so good';
+} else if (randomNumber === 9) {
+  answer = 'Very doubtful';
+} else {
+  answer = 'Error'; 
+}
+
+console.log('Question: ' + question);
+console.log('Magic 8 Ball: ' + answer);
