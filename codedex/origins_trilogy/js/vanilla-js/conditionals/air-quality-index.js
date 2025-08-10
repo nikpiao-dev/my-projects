8b46b763255d46a424cc7bb1f169238d90dc6bed
Nik <nik.piao26@gmail.com>
2025-08-10 18:32:29 -0400

    created a new txt file

---
+++ codedex/origins_trilogy/js/vanilla-js/conditionals/air-quality-index.js
@@ -0,0 +1,21 @@
+"""
+
+Instructions
+In summer 2023, the smoke from wildfires in Canada raised concerns about the Air Quality Index (AQI) in one's area.
+
+Create an air-quality-index.js program.
+
+Google the AQI in your area and define an aqi variable with that number.
+
+Write an if/else if/else statement with the following logic:
+
+If aqi is between 0 and 50, print "Good".
+Else if aqi is between 51 and 100, print "Moderate".
+Else if aqi is between 101 and 150, print "Unhealthy (Sensitive Groups)".
+Else if aqi is between 151 and 200, print "Unhealthy".
+Else if aqi is between 201 and 300, print "Very Unhealthy".
+Else, print "Hazardous".
+
+
+
+"""
