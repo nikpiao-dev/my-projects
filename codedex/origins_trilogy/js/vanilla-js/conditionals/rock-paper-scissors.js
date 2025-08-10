9d15241110e3ce4da8bbc66861331347af6549bb
Nik <nik.piao26@gmail.com>
2025-08-10 18:34:10 -0400

    added new files

---
+++ codedex/origins_trilogy/js/vanilla-js/conditionals/rock-paper-scissors.js
@@ -0,0 +1,24 @@
+"""
+
+Instructions
+
+
+Rock Paper Scissors is a classic game that resonates with folks from around the world. The rules are as follows:
+
+Rock beats Scissors.
+Scissors beat Paper.
+Paper beats Rock.
+Let's use conditionals, the random number generator, and create a program that simulates the game between the player and the computer!
+
+Begin with a player variable and give it a 0 to represent "Rock", a 1 to represent "Paper", or a 2 to represent "Scissors."
+
+Then use Math.random() to generate a number between 0 and 2 for another computer variable.
+
+Then, use conditionals to compare the values of player and computer to see who wins!
+
+Player picked:      Rock
+Computer picked:    Scissors
+
+The player won!
+
+"""
