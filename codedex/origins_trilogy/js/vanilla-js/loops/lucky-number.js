a96249c298261265ca1dcfea282a4d6d7ea981ca
Nik <nik.piao26@gmail.com>
2025-08-13 19:21:06 -0400

    new content

--- codedex/origins_trilogy/js/vanilla-js/loops/lucky-number.js
+++ codedex/origins_trilogy/js/vanilla-js/loops/lucky-number.js
@@ -19,3 +19,12 @@ My lucky number is 10!
 """
 
 
+
+const luckyNumber = 7
+let guess = Math.floor(Math.random() * 10) + 1;
+
+while (guess !== luckyNumber) {
+  console.log(`Nope, it isn't ${guess}`);
+  guess = Math.floor(Math.random() * 10) + 1; 
+}
+console.log(`My lucky number is ${luckyNumber}!`);
