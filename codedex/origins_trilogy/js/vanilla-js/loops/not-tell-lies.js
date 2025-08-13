c59a8d22b3f7243e9225e5341ffcfdd3033d728a
Nik <nik.piao26@gmail.com>
2025-08-13 19:22:37 -0400

    added new js content

---
+++ codedex/origins_trilogy/js/vanilla-js/loops/not-tell-lies.js
@@ -0,0 +1,18 @@
+"""
+Instructions
+Suppose you got detention and the professor asks you to write a phrase 100 times.
+
+Let's create a program that uses for loops to make short work of this!
+
+Use the for loop to log the following message to the Console 100 times:
+
+I Must Not Tell Lies
+
+(This is where we begin to see the true power of computing. What takes humans hours can take computer microseconds. 🤯)
+
+"""
+
+for (let i = 1; i <= 100; i++) {
+  console.log(`${i}: I Must Not Tell Lies`);
+}
+
