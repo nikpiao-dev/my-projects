8a40717d41101cca8a8ddb4babd4d674c9900676
Nik <nik.piao26@gmail.com>
2025-08-17 19:33:00 -0400

    added new python file

---
+++ codedex/legends_python/pandas/movies.py
@@ -0,0 +1,22 @@
+"""
+Instructions
+In the notebook on the right, we've imported pandas and created a DataFrame named movies!
+
+Now, type the following into the input box at the bottom of the page:
+
+movies
+
+And press shift + enter to run the code. A table should appear.
+
+Boom. We're now ready to learn how to use Pandas to answer questions about this data. 🙌
+
+Bonus: Now let’s get curious. What’s a question you might ask about this dataset?
+
+Write it as a Python comment and hit shift + enter again. For example:
+
+# What month is the best month to release a movie? What month is the worst?
+# Which film studio has the highest average profit?
+
+
+"""
+
