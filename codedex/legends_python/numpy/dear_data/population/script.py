fa3367d6cd1e797ff9346b36138edf11fcae3a9f
Nik <nik.piao26@gmail.com>
2025-08-21 18:40:35 -0400

    added new content in file

--- codedex/legends_python/numpy/dear_data/population/script.py
+++ codedex/legends_python/numpy/dear_data/population/script.py
@@ -43,3 +43,19 @@ Now find the difference between them using subtraction. How did the population c
 COVID-19 happened during 2020-2022; those were some crazy years. Use slicing, and print the population during those years for your city.
 
 """
+
+# import numpy:
+
+import numpy as np
+
+
+# set population array:
+
+population = np.array([19571216, 19673200, 19854526, 20104710, 19463131, 19544098, 19593849, 19636391, 19657321, 19653431, 19626488])
+
+print(population[0])
+print(population[10])
+
+print(population[10]-population[0])
+
+print(population[1:4])
