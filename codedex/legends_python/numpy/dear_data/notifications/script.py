5eb1348b67f60f8de6620d67592627cc79fbf6d8
Nik <nik.piao26@gmail.com>
2025-08-20 20:13:15 -0400

    added new content

---
+++ codedex/legends_python/numpy/dear_data/notifications/script.py
@@ -0,0 +1,15 @@
+"""
+
+Instructions
+Studies have found that people now get as many as 237 notifications every day on their smartphones. 🔔 Let’s see how many you got!
+
+Look through your settings:
+
+iPhone: Settings > Screen Time > See All App & Website Activity > Notifications (bottom)
+Android: Settings > Digital Wellbeing and parental controls > Notifications received
+Create an array of the number of notifications you received in the past week.
+
+Print the array out.
+
+"""
+
