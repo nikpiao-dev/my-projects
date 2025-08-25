20f05566ad25f42e46f24cc25467a815aef617f4
Nik <nik.piao26@gmail.com>
2025-08-24 20:51:05 -0400

    added new content

---
+++ codedex/legends_python/pandas/data_frame.py
@@ -0,0 +1,14 @@
+import pandas as pd 
+
+data = {
+  'name': ['Bart', 'Lisa', 'Homer', 'Marge'],
+  'age': [10, 8, 39, 36],
+  'phone_number': ['', '', '', ''],
+  'astrological_sign': ['', '', '', '']
+}
+
+contacts = pd.DataFrame(data)
+
+print(contants)
+
+
