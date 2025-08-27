fdf87e1bc7b6fee323d777a250193c7cd1052bcf
Nik <nik.piao26@gmail.com>
2025-08-26 20:13:31 -0400

    added new content

--- codedex/legends_python/numpy/dear_data/bad_eggs.py
+++ codedex/legends_python/numpy/dear_data/bad_eggs.py
@@ -29,3 +29,21 @@ Which carton should you get?
 Calculate the average freshness of each of the cartons with np.average() (which we will learn more about in chapter 2) and see which one has the lowest average (least fresh).
 
 """
+
+
+import numpy as np
+
+egg_carton1 = np.array([
+  [0.89, 0.90, 0.83, 0.89, 0.97, 0.98], 
+  [0.95, 0.95, 0.89, 0.95, 0.23, 0.99]
+])
+  
+egg_carton2 = np.array([
+  [0.89, 0.95, 0.84, 0.92, 0.94, 0.93], 
+  [0.93, 0.95, 0.02, 0.03, 0.23, 0.99]
+])
+  
+egg_carton3 = np.array([
+  [0.83, 0.95, 0.89, 0.54, 0.37, 0.92], 
+  [0.98, 0.99, 0.19, 0.23, 0.89, 0.91]
+])
