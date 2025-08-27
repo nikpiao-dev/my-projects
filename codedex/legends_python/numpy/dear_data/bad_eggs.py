fa6cad3d4e977bffd1260df5d57772c040815a72
Nik <nik.piao26@gmail.com>
2025-08-26 20:14:09 -0400

    added more content in the file

--- codedex/legends_python/numpy/dear_data/bad_eggs.py
+++ codedex/legends_python/numpy/dear_data/bad_eggs.py
@@ -47,3 +47,11 @@ egg_carton3 = np.array([
   [0.83, 0.95, 0.89, 0.54, 0.37, 0.92], 
   [0.98, 0.99, 0.19, 0.23, 0.89, 0.91]
 ])
+
+average1 = np.average(egg_carton1)
+average2 = np.average(egg_carton2)
+average3 = np.average(egg_carton3)
+
+print(average1)
+print(average2)
+print(average3)
